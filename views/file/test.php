b4fe1cd650bbe4c604b42fdf97cc7b7821136a96
secret <891841626@qq.com>
2018-07-24 18:44:29 +0800

    *

---
+++ views/file/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoxy
+ * Date: 2018/7/24
+ * Time: 18:20
+ */
+?>
+<html>
+<body>
+<form action="/file/uploads" method="post" enctype="multipart/form-data">
+    <input type="file" name="fileUpload" />
+    <input type="submit" value="上传文件" />
+</form>
+</body>
+</html>
+
