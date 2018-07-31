8b6fc5c440ee7e4b496865711b7cb4af088f5dd0
secret <891841626@qq.com>
2018-07-31 12:04:01 +0800

    *

--- controllers/SendEmailController.php
+++ controllers/SendEmailController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 
 use app\commond\Email;
+use app\commond\PHPMailer\PHPMailer;
 use Yii;
 
 
@@ -26,28 +27,52 @@ class SendEmailController extends BasicController
 
     public function actionSend()
     {
-        //主題
-        $subject = "test send email";
 
-        //收件人
-        $sendto = 'gaoxinyu@imooc.com';
+        $mail = new Email();
+        $mail->setTo("a@a.com"); //收件人
 
-        //發件人
-        $replyto = '891841626@qq.com';
+        $mail->setFrom("gaoxinyu@imooc.com");//发件人
+        $mail->setSubject("subject") ; //主题
+        $mail->setText('文本格式') ;//发送文本格式也可以是变量
+        $mail->setHTML('html格式') ;//发送html格式也可以是变量
+        $mail->setAttachments('vvv') ;//添加附件,需表明路径
+        $mail->send(); //发送邮件
+        /*
+        $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+        try {
+            //Server settings
+            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            $mail->isSMTP();
+            $mail->CharSet='utf-8';     //设置字符集// Set mailer to use SMTP
+            $mail->Host = SMTP_SERVER;  // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = SMTP_USER_NAME;                 // SMTP username
+            $mail->Password = SMTP_USER_PASS;                           // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = SMTP_SERVER_PORT;                                    // TCP port to connect to
 
-        //內容 www.jbxue.com
-        $message = "test send email content";
+            //Recipients
+            $mail->setFrom('from@example.com', 'Mailer');
+            $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+            $mail->addAddress('ellen@example.com');               // Name is optional
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
 
-        //附件
-        $filename = 'test.jpg';
+            //Attachments
+            $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-        //附件類別
-        $mimetype = "image/jpeg";
-        $excelname ='aaa.jpg';
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mailfile = new Email($subject,$sendto,$replyto,$message,$filename,$excelname,$mimetype);
-        $res = $mailfile->sendfile();
-
-        echo '<pre>';print_r($res);
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }*/
     }
 }
