c5e29f9afd84e3b67367622cacb28bbf957c39d9
secret <891841626@qq.com>
2018-07-31 15:19:02 +0800

    *

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=118.190.199.43;dbname=Achievements',
-    'username' => 'root',
-    'password' => 'sycalc204432CH',
-    'charset' => 'utf8',
-];
