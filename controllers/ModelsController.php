266bd15e557639fa79cac62096f7fb3884c5ce75
secret <891841626@qq.com>
2018-08-11 21:18:45 +0800

    *

--- controllers/ModelsController.php
+++ controllers/ModelsController.php
@@ -23,12 +23,8 @@ class ModelsController extends BasicController
      */
     public function actionIndex(){
        // $this->isPost();
+
         $uid = $this->getParam('userId',true);
-/*
-        $user = AUser::find()->select('group')->where(['id'=>$uid,'status'=>0])->scalar();
-        if ($user && $user == 1){
-            $uid = 1;
-        }*/
         $data = AModel::find()->select('id,name,pid')
             ->where(['status'=>0,'project_id'=>0])->asArray()->all();
         
@@ -74,6 +70,16 @@ class ModelsController extends BasicController
         $Obj->level = $level;
 
         if ($Obj->insert()) {
+            $msg = '操作人:';
+            $msg .= AUser::getName($createUid);
+            if ($type == 0) {
+                $msg.= '创建模板:'.$name;
+                helps::writeLog('模板日志',$msg);
+            } else {
+                $msg.= '创建目录:'.$name;
+                helps::writeLog('目录日志',$msg);
+            }
+
             $this->Success(['id'=>$Obj->attributes['id']]);
         }
         $this->Error(Constants::RET_ERROR,Constants::$error_message[Constants::RET_ERROR]);
@@ -85,18 +91,28 @@ class ModelsController extends BasicController
     public function actionEdit()
     {
         $this->isPost();
-        $id      = $this->getParam('id',true);
+        $id   = $this->getParam('id',true);
         $name = $this->getParam('name',true);
+        $uid  = $this->getParam('userId',true);
 
         $Obj = AModel::findOne($id);
 
         if (!$Obj) {
             $this->Error(Constants::DATA_NOT_FOUND,Constants::$error_message[Constants::DATA_NOT_FOUND]);
         }
-
+        $oldName = $Obj->name;
         $Obj->name = $name;
         $Obj->update_time = time();
         if ($Obj->save(false)) {
+            $msg = '操作人:';
+            $msg .= AUser::getName($uid);
+            if ($Obj->type == 0) {
+                $msg.= '编辑模板:'.$oldName.'改成'.$name;
+                helps::writeLog('模板日志',$msg);
+            } else {
+                $msg.= '编辑目录:'.$oldName.'改成'.$name;
+                helps::writeLog('目录日志',$msg);
+            }
             $this->Success();
         }
 
@@ -110,7 +126,8 @@ class ModelsController extends BasicController
     public function actionDel()
     {
         $this->isPost();
-        $id          = $this->getParam('id',true);
+        $id  = $this->getParam('id',true);
+        $uid = $this->getParam('userId',true);
         $Obj = AModel::findOne($id);
 
         if (!$Obj) {
@@ -119,6 +136,15 @@ class ModelsController extends BasicController
 
         $Obj->status = -1;
         if ($Obj->save(false)) {
+            $msg = '操作人:';
+            $msg .= AUser::getName($uid);
+            if ($Obj->type == 0) {
+                $msg.= '删除模板:'.$Obj->name;
+                helps::writeLog('模板日志',$msg);
+            } else {
+                $msg.= '删除目录:'.$Obj->name;
+                helps::writeLog('目录日志',$msg);
+            }
             $this->Success();
         }
 
@@ -131,9 +157,10 @@ class ModelsController extends BasicController
      */
     public function actionAddRemark(){
 
-        //$this->isPost();
+        $this->isPost();
         $modelId = $this->getParam('modelId',true);
-        $remark = $this->getParam('remark',true);
+        $remark  = $this->getParam('remark',true);
+        $uid     = $this->getParam('userId',true);
 
         $model = AModel::findOne(['id'=>$modelId,'status'=>0]);
 
@@ -145,6 +172,10 @@ class ModelsController extends BasicController
         $model->update_time = time();
 
         if ($model->save(false)) {
+            $msg = '操作人:';
+            $msg .= AUser::getName($uid);
+            $msg.= '模块:'.$model->name.'添加备注:'.$remark;
+            helps::writeLog('模板日志',$msg);
             $this->Success();
         }
 
