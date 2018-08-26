909780d6b01904478e0b20f97d8c7aaa3ea92a3d
secret <891841626@qq.com>
2018-08-26 21:03:49 +0800

    @

--- controllers/SendEmailController.php
+++ controllers/SendEmailController.php
@@ -138,7 +138,7 @@ class SendEmailController extends BasicController
         if($sendEmail->save(false)) {
             error_log('pack_file'.date('Y-m-d H:i:s').'#####'.$zipName
                 .PHP_EOL,3,'/tmp/packfile.log');
-            @unlink($projectPath);
+            //@unlink($projectPath);
             $this->Success();
         } else {
             $this->Error(Constants::PROJECT_PACK_FAIL,Constants::$error_message[Constants::PROJECT_PACK_FAIL]);
