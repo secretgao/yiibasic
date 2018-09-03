bfa09a60c77006174f752efd53f4ce4f81993fa2
secret <891841626@qq.com>
2018-09-03 11:37:06 +0800

    *

--- config/params.php
+++ config/params.php
@@ -14,7 +14,7 @@ return [
         1=>'1.5.2',   //ios
         2=>[
             'num'=> '1.5.0',   //安卓
-            'url'=> 'http://118.190.199.43/uploads/install/app-release_111_jiagu_sign.apk.1',
+            'url'=> 'http://118.190.199.43/uploads/install/app-release_150_1_360应用平台_sign.apk.1',
             'message'=>'更新bug',
         ]
     ],
