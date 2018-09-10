f5f41b657774cbc2e70fb7a7d0a00d44edcecb64
891841626 <891841626@qq.com>
2018-09-10 22:20:19 +0800

    3

--- config/params.php
+++ config/params.php
@@ -14,8 +14,8 @@ return [
         1=>'1.5.4',   //ios
         2=>[
             'num'=> '1.5.2',   //安卓
-            'url'=> 'http://118.190.199.43/uploads/install/360.apk.1',
-            'message'=>"1 优化用户体验\r\n2 修复部分bug",
+            'url'=> 'http://118.190.199.43/uploads/install/app_152_1_sign.apk.1',
+            'message'=>"1 优化用户体验\r\n2 修复部分bug\r\n 3 项目列表增加根据一级模版筛选",
         ]
     ],
 
