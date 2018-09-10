e76886f4b6905815b8947159e447cbb0ff7c7e25
891841626 <891841626@qq.com>
2018-09-10 22:14:12 +0800

    #

--- config/params.php
+++ config/params.php
@@ -13,7 +13,7 @@ return [
     'version' =>[
         1=>'1.5.4',   //ios
         2=>[
-            'num'=> '1.5.1',   //安卓
+            'num'=> '1.5.2',   //安卓
             'url'=> 'http://118.190.199.43/uploads/install/360.apk.1',
             'message'=>"1 优化用户体验\r\n2 修复部分bug",
         ]
