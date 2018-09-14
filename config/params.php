b69828791fb33d8aabb4648a7a2f158d9af726c0
secret <891841626@qq.com>
2018-09-14 09:27:57 +0800

    *

--- config/params.php
+++ config/params.php
@@ -13,9 +13,9 @@ return [
     'version' =>[
         1=>'1.5.6',   //ios
         2=>[
-            'num'=> '1.5.2',   //安卓
-            'url'=> 'http://118.190.199.43/uploads/install/app_152_1_sign.apk.1',
-            'message'=>"项目筛选功能优化",
+            'num'=> '1.5.3',   //安卓
+            'url'=> 'http://118.190.199.43/uploads/install/app-153_1_sign.apk.1',
+            'message'=>"1 首页管理员身份检索优化，添加分类，部门排列\r\n2 我的界面添加意见反馈\r\n3 项目详情界面不再展示一级模板分类\r\n4 地图定位优化",
         ]
     ],
 
