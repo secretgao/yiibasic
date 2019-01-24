a55af3d7cd63b5e29ae1875651bf29d7be4a5a80
secret <891841626@qq.com>
2019-01-24 11:03:38 +0800

    *

--- config/params.php
+++ config/params.php
@@ -14,7 +14,7 @@ return [
         1=>'1.6.1',   //ios
         2=>[
             'num'=> '1.6.1',   //安卓
-            'url'=> 'http://118.190.199.43/uploads/install/app-161-sign.apk.1',
+            'url'=> 'http://www.sycalc.com.cn/uploads/install/app-161-sign.apk.1',
             'message'=>"1 添加项目搜索\r\n2 项目列表分页，上拉加载数据\r\n3 当前分类项目总数显示\r\n4 文件夹有无文件图标状态显示\r\n5 添加SSL安全证书",
         ]
     ],
